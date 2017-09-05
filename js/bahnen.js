08f4a7c00e91de9a571273309a114d9fbde9c8e3
lukas <lukas.mueller96@gmail.com>
2017-09-05 11:52:27 +0200

    Bahneditor function hinzugefügt

--- js/bahnen.js
+++ js/bahnen.js
@@ -74,9 +74,9 @@ function init() {
     ctx = canvas.getContext('2d');
 
     //background image
-    var pic = new Image();
+    /*var pic = new Image();
     pic.src = "images/bahn1.jpg";
-    pic.addEventListener("load", function () {ctx.drawImage(pic, 0, 0)}, false);
+    pic.addEventListener("load", function () {ctx.drawImage(pic, 0, 0)}, false);*/
 
     ctx.strokeStyle = 'yellow';
     ctx.lineWidth = 6;
@@ -91,57 +91,8 @@ function init() {
 window.addEventListener('load', init, false);
 //mouse-draw Funktion ENDE
 
-
-
-
-
-
-
-/*var canvas = document.getElementById('canvas');
-
-canvas.width = canvas.scrollWidth;
-canvas.height=canvas.scrollHeight;
-var ctx=canvas.getContext('2d');
-
-var clicks = 0;
-var lastClick = [0, 0];
-
-document.getElementById('canvas').addEventListener('click', drawLine, false);
-
-function getCursorPosition(e) {
-    var x;
-    var y;
-
-    if (e.pageX != undefined && e.pageY != undefined) {
-        x = event.pageX + canvas.offsetLeft,
-        y = event.pageY + canvas.offsetTop;
-    } else {
-        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-    }
-
-    return [x, y];
+function bahn($bahntyp){
+    var pic = new Image();
+    pic.src = "images/bahn"+$bahntyp+".jpg";
+    pic.addEventListener("load", function () {ctx.drawImage(pic, 0, 0)}, false);
 }
-
-function drawLine(e) {
-    context = this.getContext('2d');
-
-    x = getCursorPosition(e)[0] - this.offsetLeft;
-    y = getCursorPosition(e)[1] - this.offsetTop;
-
-    if (clicks != 1) {
-        clicks++;
-    } else {
-        context.beginPath();
-        context.moveTo(lastClick[0], lastClick[1]);
-        context.lineTo(x, y, 6);
-
-        context.strokeStyle = '#000000';
-        context.stroke();
-
-        clicks = 0;
-    }
-
-    lastClick = [x, y];
-};
-*/
