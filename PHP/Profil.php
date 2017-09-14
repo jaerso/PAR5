db785df0cceac29da891b35bebbf6ae7507ef222
conny92 <conny92@users.noreply.github.com>
2017-09-14 15:39:45 +0200

    profilseite geändert

--- PHP/Profil.php
+++ PHP/Profil.php
@@ -2,86 +2,32 @@
 include_once 'includes/dbh.inc.php';
 include_once 'includes/picture.inc.php';
 
-<<<<<<< HEAD
 
 echo "<div class='profil-container'>";
 
-echo "<h1>Dein Profil</h1>";
-	$sql = "SELECT * FROM users";
-	$result = mysqli_query($conn, $sql);
-	if (mysqli_num_rows($result) > 0) {
-		while ($row = mysqli_fetch_assoc($result)) {
-			$id = $row['user_id'];
-			$sqlImg = "SELECT * FROM profileimg WHERE userid='$id'";
-			$resultImg = mysqli_query($conn, $sqlImg);
-			while($rowImg = mysqli_fetch_assoc($resultImg)) {
-				echo "<div class='user-container'>";
-					if ($rowImg['status'] == 0) {
-						$filename = "uploads/profile".$id."*";
-						$fileinfo=glob($filename);
-						$fileext=explode(".",$fileinfo[0]);
-						$fileactualext= $fileext[1];
-						//Profilausgabe
-						echo "<img src='uploads/profile".$id.".".$fileactualext."?".mt_rand()."'>";
-					} else {
-						echo "<img src='uploads/profiledefault.jpg'>";
-					}
-				
-					
-					if (isset($_SESSION['u_id'])) {
-						echo "<form action='includes/upload.inc.php' method='POST' enctype='multipart/form-data'>
-							<input type='file' name='file'>
-							<button type='submit' name='submit'>Hochladen</button>
-						</form>";
-						echo "<form action='includes/deleteprofile.inc.php' method='POST'>
-						<button type='submit' name='submit'>Profilbild löschen</button>
-					</form>";
-					} 
-					
-					function profilIcon(){
-						$username= $_SESSION['u_uid'];
-					   /* $id=$_SESSION['u_id'];
-						echo "<img src='uploads/profile".$id.".".$fileactualext."?".mt_rand()."'>";*/
-						echo "Hallo <a href='index.php?page=profile'>$username</a>";
-					   
-					}	
 
-
-
-				echo "</div>";
-
-
-
-					echo "<p> Dein Benutzername: ".$row['user_uid']."</p>";
-					echo "<p> Dein Vor- und Nachname: ".$row['user_first'] ." ". $row['user_last']."</p>";
-					
-
-
-
-				echo "</div>";
-				//Profilausgabe Ende
-			}
-		}
-	}
-
-=======
 //Profilausgabe Ende
 echo "<h1>Profil</h1>";
 echo "<div class='user-container'>";
 echo "<img src=$profilepic>";
-echo "<p>".$username."</p>";
-echo "<p>".$firstname." ". $lastname."</p>";
-echo "</div>";
-
-		if (isset($_SESSION['u_id'])) {
+	if (isset($_SESSION['u_id'])) {
 		echo "<form action='includes/upload.inc.php' method='POST' enctype='multipart/form-data'>
 			<input type='file' name='file'>
-			<button type='submit' name='submit'>UPLOAD</button>
+			<button type='submit' name='submit'>hochladen</button>
 		</form>";
 		echo "<form action='includes/deleteprofile.inc.php' method='POST'>
-		<button type='submit' name='submit'>Delete profile image</button>
+		<button type='submit' name='submit'>Lösche dein Profilbild</button>
 	</form>";
 		}
->>>>>>> a7f744154a442d816731dd54db3cea91923b2266
+	
+echo "</div>";
+
+	
+	
+	
+		echo "<p> Dein Benutzername: ".$username."</p>";
+		echo "<p> Dein Vor- und Nachname".$firstname." ". $lastname."</p>";
+
+ echo "</div>";
 
 ?>
