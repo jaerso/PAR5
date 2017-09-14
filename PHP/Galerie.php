9ad8442c7aea9e9c1754e6c6198d694aaae41928
JHoeppe <jhoeppe@users.noreply.github.com>
2017-09-14 14:00:21 +0200

    gallery Anpassungen

--- PHP/Galerie.php
+++ PHP/Galerie.php
@@ -55,9 +55,9 @@ $fotos = [
                     <img src="images/gallery/miniaturen/<?=htmlspecialchars($foto['Dateiname']); ?>" alt="<?=htmlspecialchars($foto['Dateiname']); ?>">
                 </a>
             </div>
-            <p> Schlagempfehlungen <?=htmlspecialchars($foto['Bahnnummer']); ?> </p>
-            <a href="index.php?page=gallery&bahn=Schlagempfehlungen/<?=htmlspecialchars($foto['Bahnnummer']); ?>">
-                Schlagempfehlungen
+            
+            <a href="index.php?page=gallery&bahn=images/gallery/Schlagempfehlungen/<?=htmlspecialchars($foto['Bahnnummer']); ?>">
+            Schlagempfehlungen <?=htmlspecialchars($foto['Bahnnummer']); ?>
             </a>
             <div class="clearfix"></div>
         </div>
@@ -70,18 +70,18 @@ $fotos = [
         echo"<h2> Schlagempfehlungen für Bahn ".$a. " <h2>";
         
 
-        $elemente = scandir('images/gallery/'.$bildOrdner);
+        $elemente = scandir($bildOrdner);
         foreach($elemente as $e){
             if($e == '.' or $e == '..') {continue; }
             //if(is_dir($bildOrdner.$e)){
             //für unterordner   $html .= '<a href="index.php?album='.urlencode($bildOrdner.$e.'/').'">'.$e.'</a>';
             //}
             else {
-                $size = getimagesize('images/gallery/'.$bildOrdner."/".$e);
-                if($size[2] == 1 or $size[2] == 2) {
+                $size = getimagesize($bildOrdner."/".$e);
+                if($size[2] == 3 or $size[2] == 2) {
                     $html .= '<div style="display:inline-block;margin:7px;">
                     <img src="'.$bildOrdner."/".$e.'" '.$size[3].' alt="'.$e.'"></div>';
-                    //echo $bildOrdner;
+                    //echo $e;
                 }
             }
 
