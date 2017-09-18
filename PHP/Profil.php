b9f4cd5d0a8a779e9c76d664220340bde103baf2
DominikSommer <29626701+DominikSommer@users.noreply.github.com>
2017-09-18 14:29:28 +0200

    Kommentar/Galerie

--- PHP/Profil.php
+++ PHP/Profil.php
@@ -2,69 +2,6 @@
 include_once 'includes/dbh.inc.php';
 include_once 'includes/picture.inc.php';
 
-<<<<<<< HEAD
-
-echo "<div class='profil-container'>";
-
-echo "<h1>Dein Profil</h1>";
-	$sql = "SELECT * FROM users";
-	$result = mysqli_query($conn, $sql);
-	if (mysqli_num_rows($result) > 0) {
-		while ($row = mysqli_fetch_assoc($result)) {
-			$id = $row['user_id'];
-			$sqlImg = "SELECT * FROM profileimg WHERE userid='$id'";
-			$resultImg = mysqli_query($conn, $sqlImg);
-			while($rowImg = mysqli_fetch_assoc($resultImg)) {
-				echo "<div class='user-container'>";
-					if ($rowImg['status'] == 0) {
-						$filename = "uploads/profile".$id."*";
-						$fileinfo=glob($filename);
-						$fileext=explode(".",$fileinfo[0]);
-						$fileactualext= $fileext[1];
-						//Profilausgabe
-						echo "<img src='uploads/profile".$id.".".$fileactualext."?".mt_rand()."'>";
-					} else {
-						echo "<img src='uploads/profiledefault.jpg'>";
-					}
-				
-					
-					if (isset($_SESSION['u_id'])) {
-						echo "<form action='includes/upload.inc.php' method='POST' enctype='multipart/form-data'>
-							<input type='file' name='file'>
-							<button type='submit' name='submit'>Hochladen</button>
-						</form>";
-						echo "<form action='includes/deleteprofile.inc.php' method='POST'>
-						<button type='submit' name='submit'>Profilbild löschen</button>
-					</form>";
-					} 
-					
-					function profilIcon(){
-						$username= $_SESSION['u_uid'];
-					   /* $id=$_SESSION['u_id'];
-						echo "<img src='uploads/profile".$id.".".$fileactualext."?".mt_rand()."'>";*/
-						echo "Hallo <a href='index.php?page=profile'>$username</a>";
-					   
-					}	
-
-
-
-				echo "</div>";
-
-
-
-					echo "<p> Dein Benutzername: ".$row['user_uid']."</p>";
-					echo "<p> Dein Vor- und Nachname: ".$row['user_first'] ." ". $row['user_last']."</p>";
-					
-
-
-
-				echo "</div>";
-				//Profilausgabe Ende
-			}
-		}
-	}
-
-=======
 //Profilausgabe Ende
 echo "<h1>Profil</h1>";
 echo "<div class='user-container'>";
@@ -82,6 +19,5 @@ echo "</div>";
 		<button type='submit' name='submit'>Delete profile image</button>
 	</form>";
 		}
->>>>>>> a7f744154a442d816731dd54db3cea91923b2266
 
 ?>
---
+++ includes/comments.inc.php
@@ -0,0 +1,149 @@
+<?php
+
+function setComments($conn){
+if(isset($_POST['commentSubmit'])){
+$uid=$_POST['uid'];
+$date=$_POST['date'];
+$message=$_POST['message'];
+
+$sql = "INSERT INTO comments (uid, date, message) VALUES('$uid', '$date', '$message')";
+$result = mysqli_query($conn,$sql);
+}
+}
+
+function getComments($conn){
+    $sql = "SELECT * FROM comments ORDER BY date DESC";            //go into the database
+    $result = mysqli_query($conn, $sql);        //run the query
+    while($row = mysqli_fetch_assoc($result)){  //spit it out
+        $id = $row['uid'];
+        $sql2 = "SELECT * FROM users WHERE user_id='$id'";
+        $result2 = mysqli_query($conn, $sql2);
+        if($row2 = mysqli_fetch_assoc($result2)){
+                echo "<div class='comment-box'><p>";
+                echo $row2['user_uid']."<br>";
+                echo $row['date']."<br>";
+                echo nl2br($row['message']); //interpretiert Absätze in sql zu php
+               // print_r($row2);
+            echo "</p>";
+            if(isset($_SESSION['u_id'])){
+                if($_SESSION['u_id']== $row2['user_id']){
+                    $_SESSION['bahn']= $_GET['bahn'];
+                    
+                    //echo "$bahn";
+                        echo "<form class='delete-form' method='POST' action='".deleteComments($conn)."'>
+                        <input type='hidden' name='uid' value='".$row['cid']."'>
+                        <button type='submit' name='commentDelete'>Löschen</button>
+                        </form>
+                        <form class='edit-form' method='POST' action='includes/editcomment.inc.php'>
+                        <input type='hidden' name='cid' value='".$row['cid']."'>
+                        <input type='hidden' name='uid' value='".$row['uid']."'>
+                        <input type='hidden' name='date' value='".$row['date']."'>
+                        <input type='hidden' name='message' value='".$row['message']."'>
+                        <button>Bearbeiten</button>
+                    
+                        </form>";
+                } /*else{
+                    echo "<form class='edit-form' method='POST' action='replycomment.php'>
+                    <input type='hidden' name='cid' value='".$row['cid']."'>
+                    <input type='hidden' name='uid' value='".$row['uid']."'>
+                    <input type='hidden' name='date' value='".$row['date']."'>
+                    <input type='hidden' name='message' value='".$row['message']."'>
+                    <button>Antworten</button>
+                    </form>";
+                }*/
+            }else{
+            echo "<p class='commentmessage'>Du musst eingeloggt sein um zu antworten</p>";  
+            }
+            echo "</div>";
+        }
+       
+    }
+  
+}
+
+
+function editComments($conn){
+    if(isset($_POST['commentSubmit'])){
+    $cid=$_POST['cid'];
+    $uid=$_POST['uid'];
+    $date=$_POST['date'];
+    $message=$_POST['message'];
+    $bahn=$_SESSION['bahn'];
+    
+    $sql = "UPDATE comments SET message='$bahn' WHERE cid='$cid'";
+    $result = mysqli_query($conn,$sql);
+    header("Location: index.php?page=gallery&bahn=$bahn");
+    
+    }
+    }
+
+
+    /*function replyComments($conn){
+        if(isset($_POST['commentReply'])){
+        $cid=$_POST['cid'];
+        $uid=$_POST['uid'];
+        $date=$_POST['date'];
+        $message=$_POST['message'];
+        
+        $sql = "INSERT INTO comments (uid, date, message) VALUES('$uid', '$date', '$message')";
+        $result = mysqli_query($conn,$sql);
+        header("Location: index.php");
+        }
+        }*/
+
+    function deleteComments($conn){
+        if(isset($_POST['commentDelete'])){
+            $cid=$_POST['cid'];
+            
+            $sql = "DELETE FROM comments WHERE cid='$cid'";
+            $result = mysqli_query($conn,$sql);
+            header("Location: index.php");
+            }
+    }
+
+    /*function getLogin($conn){
+        if(isset($_POST['loginSubmit'])){
+        $uid = mysqli_real_escape_string($conn, $_POST['uid']); 
+        $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);//run this data not as SQL code
+
+
+        //start prepared statements
+            $stmt = $conn->prepare("SELECT * FROM user WHERE uid=? AND pwd=?");
+            $stmt->bind_param("ss", $username, $password);
+
+            $username=$uid;
+            $password=$pwd;
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            $rowNum = $result->num_rows;
+
+         //end prepared statements
+
+            //möglicher Hackerangriff
+            $sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='' or '1'= '1'";
+           
+            if($rowNum > 0){
+                if ($row = mysqli_fetch_assoc($result)){
+                    $_SESSION['id'] =$row['id'];
+                    $_SESSION['username'] = $row['uid'];
+                    header("Location: index.php?loginsuccess");
+                    exit();
+                    }
+                } else{
+                    header("Location: index.php?loginfailed");
+                    exit();
+                }
+        
+            }   
+        }
+
+            function userLogout(){
+                if(isset($_POST['logoutSubmit'])){
+                    session_start();
+                    session_unset();
+                    session_destroy();
+                    header("Location: index.php");
+                    exit();
+                }
+        }*/
