39cf7241385a5fc6dc6eeadf393df4c38f37ca6b
lukas <lukas.mueller96@gmail.com>
2017-09-18 11:11:47 +0200

    Validierung der Registrierung

--- PHP/Registrierung.php
+++ PHP/Registrierung.php
@@ -1,9 +1,9 @@
 <h2>Registrierung</h2>
 		<form class="signup-form" action="includes/signup.inc.php" method="POST">
-			<input type="text" name="first" placeholder="Vorname">
-			<input type="text" name="last" placeholder="Nachname">
-			<input type="text" name="email" placeholder="E-mail">
-			<input type="text" name="uid" placeholder="Benutzername">
-			<input type="password" name="pwd" placeholder="Passwort">
+			<input id="vorname" type="text" name="first" placeholder="Vorname" maxlength="10" minlength="3" required>
+			<input type="text" name="last" placeholder="Nachname" maxlength="12" minlength="3" required >
+			<input type="email" name="email" placeholder="E-mail" required>
+			<input type="text" name="uid" placeholder="Benutzername" maxlength="10" minlength="5" required oninvalid="this.setCustomValidity('Benutzname muss mindestens 5 Zeichen haben!')">
+			<input type="password" name="pwd" placeholder="Passwort" minlength="8" required oninvalid="this.setCustomValidity('Passwort zu kurz. Bitte mindestens 8 Zeichen!')">
 			<button type="submit" name="submit">Und los!</button>
 		</form>
