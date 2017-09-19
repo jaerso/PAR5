8b7e74319044d57efc210897abae7921406e5884
lukas <lukas.mueller96@gmail.com>
2017-09-19 14:34:15 +0200

    Editor Anleitung angepasst

--- PHP/Editor.php
+++ PHP/Editor.php
@@ -1,41 +1,41 @@
-<div class="row">
-                            <div class="col-lg-3">
-                                <h1 id="editor"> Bahneditor&nbsp; <i class="fa fa-cubes" aria-hidden="true"></i></h1>
-                            </div>
-                            <div class="col-lg-3">
-                                <div class="dropdown">
-                                    <button id="dropBahn" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Bahntyp&nbsp;
-                                    <i class="fa fa-plus-square" aria-hidden="true"></i></button>
-                                    <ul class="dropdown-menu">
-                                        <a href="javascript:bahn(1)"><li>Bahn 1</li></a>
-                                        <a href="javascript:bahn(2)"><li>Bahn 2</li></a>
-                                        <a href="javascript:bahn(3)"><li>Bahn 3</li></a>
-                                        <a href="javascript:bahn(4)"><li>Bahn 4</li></a>
-                                        <a href="javascript:bahn(5)"><li>Bahn 5</li></a>
-                                        <a href="javascript:bahn(6)"><li>Bahn 6</li></a>
-                                        <a href="javascript:bahn(7)"><li>Bahn 7</li></a>
-                                        <a href="javascript:bahn(8)"><li>Bahn 8</li></a>
-                                        <a href="javascript:bahn(9)"><li>Bahn 9</li></a>
-                                        <a href="javascript:bahn(10)"><li>Bahn 10</li></a>
-                                        <a href="javascript:bahn(11)"><li>Bahn 11</li></a>
-                                        <a href="javascript:bahn(12)"><li>Bahn 12</li></a>
-                                        <a href="javascript:bahn(13)"><li>Bahn 13</li></a>
-                                        <a href="javascript:bahn(14)"><li>Bahn 14</li></a>
-                                        <a href="javascript:bahn(15)"><li>Bahn 15</li></a>
-                                        <a href="javascript:bahn(16)"><li>Bahn 16</li></a>
-                                        <a href="javascript:bahn(17)"><li>Bahn 17</li></a>
-                                        <a href="javascript:bahn(18)"><li>Bahn 18</li></a>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                 <h2 id="bahn-title"></h2>
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <h1 id="editor"> Bahneditor&nbsp; <i class="fa fa-cubes" aria-hidden="true"></i></h1>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="dropdown">
+                                <button id="dropBahn" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Bahntyp&nbsp;
+                                <i class="fa fa-plus-square" aria-hidden="true"></i></button>
+                                <ul class="dropdown-menu">
+                                    <a href="javascript:bahn(1)"><li>Bahn 1</li></a>
+                                    <a href="javascript:bahn(2)"><li>Bahn 2</li></a>
+                                    <a href="javascript:bahn(3)"><li>Bahn 3</li></a>
+                                    <a href="javascript:bahn(4)"><li>Bahn 4</li></a>
+                                    <a href="javascript:bahn(5)"><li>Bahn 5</li></a>
+                                    <a href="javascript:bahn(6)"><li>Bahn 6</li></a>
+                                    <a href="javascript:bahn(7)"><li>Bahn 7</li></a>
+                                    <a href="javascript:bahn(8)"><li>Bahn 8</li></a>
+                                    <a href="javascript:bahn(9)"><li>Bahn 9</li></a>
+                                    <a href="javascript:bahn(10)"><li>Bahn 10</li></a>
+                                    <a href="javascript:bahn(11)"><li>Bahn 11</li></a>
+                                    <a href="javascript:bahn(12)"><li>Bahn 12</li></a>
+                                    <a href="javascript:bahn(13)"><li>Bahn 13</li></a>
+                                    <a href="javascript:bahn(14)"><li>Bahn 14</li></a>
+                                    <a href="javascript:bahn(15)"><li>Bahn 15</li></a>
+                                    <a href="javascript:bahn(16)"><li>Bahn 16</li></a>
+                                    <a href="javascript:bahn(17)"><li>Bahn 17</li></a>
+                                    <a href="javascript:bahn(18)"><li>Bahn 18</li></a>
+                                </ul>
                             </div>
                         </div>
+                        <div class="col-lg-4">
+                             <h2 id="bahn-title"></h2>
+                        </div>
+                    </div>
 
-                            <canvas id='canvas'>
-                            Sorry, your browser does not support the canvas tag.
-                            </canvas>
+                    <canvas id='canvas'>
+                    Sorry, your browser does not support the canvas tag.
+                    </canvas>
 
                     <div class="col-lg-2 col-lg-push-1">
 					    <button id="clearCanvas" class="btn btn-default">Zurücksetzten</button>
@@ -50,47 +50,36 @@
                         <input name="bahn" id='bahn' type="hidden"/>
                     </form>
 
-                    <p1> Drücke die "ESC" Taste um das Zeichnen zu beenden.<br>
-					Drücke die &#9003 Taste um die Zeichenfläche zu säubern.</p1>
+                    <div class="col-lg-6 col-lg-push-1 col-lg-pull-5">
+                        <p> Drücke die "ESC" Taste um das Zeichnen zu beenden.<br>
+                        Drücke die &#9003 Taste um die Zeichenfläche zu säubern.</p>
+                    </div>
 
-					</div>
-                </div>
+                    <div class="col-lg-12">
+                        <hr style="height: 6px;	background: url(http://ibrahimjabbari.com/english/images/hr-12.png) repeat-x 0 0; border: 0;">
+                        <h2>Anleitung</h2>
+                    </div>
+
+                    <div class="col-lg-6">
+                            <h3>Bahnauswahl</h3>
+                            <p>Sie können aus dem Dropdown Menü 18 genormte Bahnen wählen und diese in den Editor laden.</p>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3>Zeichnen</h3>
+                            <p>Mit der Maus können sie auf die Bahn eine Schlagempfehlung einzeichnen.</p>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3>Speichern</h3>
+                            <p>Über den Upload-Button wird die Schlagempfehlung zu unserer Gallerie hinzugefügt.</p>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3>Zurücksetzten</h3>
+                            <p>Über den Zurücksetzten Button kann ihre Zeichnung wieder rückgängig gemacht werden.</p>
+                        </div>
+                        <div class="col-lg-12">
+                            <h4 style="text-align: center">Eine genauere Beschreiben der Funktionen finden Sie auf der <a href=index.php?page=function>Funktionsseite!</a></h4>
+                        </div>
+                    </div>
             </div>
 
-            <header class="major">
-<section id="two" class="wrapper style2 special">
-      				<div class="container">
-<h1>Anleitung</h1>
-					  <h3>Bahnauswahl</h3>
-      					<p1>
-						 Sie können aus einem "Dropdown-Menü" 18 verschieden genormten Bahnen wähle.
-						 Sobald Sie eine Bahn ausgewählt haben, wird diese in den Editor geladen.
-						</p1>
-      					
-					<h3>Zeichnen</h3>
-      					<p1>
-						 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-						 tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero 
-						 eos et accusam et justo duo dolores et ea.
-						</p1>
-						<h3>Speichern</h3>
-      					<p1>
-						 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
-						 tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero 
-						 eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
-						 takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-						</p1>
-						<h3>Zurückstzten"</h3>
-      					<p1>
-						 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
-						 tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero 
-						 eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
-						 takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-						</p1>
-      				</div>
-<div class="container3">					 
-<p2>Eine genauere Beschreiben der Funktionen finden Sie auf der <a href=index.php?page=function>Funktionsseite!</a></p2>
-<div>
-      			</section>
-                </header>
-</div>
+
