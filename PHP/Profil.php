95931ed76c17ee0ae4741e42a963f066dbdf2d17
conny92 <conny92@users.noreply.github.com>
2017-09-19 14:04:27 +0200

    profil anpassen

--- PHP/Profil.php
+++ PHP/Profil.php
@@ -3,20 +3,23 @@ include_once 'includes/dbh.inc.php';
 include_once 'includes/picture.inc.php';
 
 
-echo "<div class='profil-container'>";
+echo "<div class='profil-container'><img src='images/profil2.jpeg' 
+style='margin-left:60px; opacity: 0.5;'>
+<div class='user-container'>";
 
 
 //Profilausgabe Ende
-echo "<h1 style='color: grey;'>Profil</h1>";
-echo "<div class='user-container'>";
+
+
+
 echo "<div class='img-responsive'> <img class='zoom' src=$profilepic> </div>";
 	if (isset($_SESSION['u_id'])) {
-		echo "<form action='includes/upload.inc.php' method='POST' enctype='multipart/form-data'>
+		echo "<form action='includes/upload.inc.php' method='POST' enctype='multipart/form-data' style='margin-left:230px;'>
 		<p>Wähle ein neues Profilbild aus <input type='file' name='file'> </p>
-		<button type='submit' name='submit'style='border-radius: 8px; padding:0 4px 0 4px; margin: 0 10px 0 10px;'> 📷 hochladen </button>
+		<button type='submit' name='submit'style='border-radius: 8px; padding:0 4px 0 4px; margin: 0 230px 0 0;'> 📷 hochladen </button>
 		</form>";
 		echo "<form action='includes/deleteprofile.inc.php' method='POST'>
-		<button type='submit' name='submit' style='border-radius: 8px; padding:0 4px 0 4px; margin: 0 10px 0 10px; '>🗑️ löschen </button>
+		<button type='submit' name='submit' style='border-radius: 8px; padding:0 4px 0 4px; margin: 0 230px 0 230px; '>🗑️ löschen </button>
 	</form>";
 		}
 	
@@ -25,8 +28,8 @@ echo "</div>";
 	
 	
 	
-		echo "<p style='color: grey; font-weight:bold; margin:1em  0 0 1em;'> Dein Benutzername: ".$username."</p>";
-		echo "<p style='color: grey; font-weight:bold; margin:1em  0 0 1em;'> Dein Name: ".$firstname." ". $lastname."</p>";
+		echo "<p style='color: grey; font-weight:bold; margin:1em  0 0 1em; padding: 0 2.5em 0 2.5em;'> Dein Benutzername: ".$username."</p>";
+		echo "<p style='color: grey; font-weight:bold; margin:1em  0 0 1em; padding: 0 2.5em 0 2.5em;'> Dein Name: ".$firstname." ". $lastname."</p>";
 
  echo "</div>";
 
