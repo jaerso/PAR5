20387e0c1f91c41a438189492559e6123164df0e
JHoeppe <jhoeppe@users.noreply.github.com>
2017-09-28 11:18:56 +0200

    automatischer Upload schlagempfehlungen

--- saveCanvasDataUrl.php
+++ saveCanvasDataUrl.php
@@ -1,17 +1,21 @@
 <?php
 
-include 'dbh.inc.php';
+include_once 'includes/dbh.inc.php';
 
 $bahn = $_POST['bahn'];
-    $upload_dir = "images/gallery/Schlagempfehlungen/Bahn".$bahn."/";
+$upload_dir = "images/gallery/Schlagempfehlungen/Bahn".$bahn."/";
 $img = $_POST['hidden_data'];
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
-$save_name = 'images';
+$save_name = "images";
 $bild_name = mktime() . ".png";
 $file = $upload_dir . $bild_name ;
 $success = file_put_contents($file, $data);
-mysqli_query($conn ,"INSERT INTO ".$save_name." SET name = '".$bild_name."',type = 'image/png' ,bildlink = '".$upload_dir."/".$bild_name."',bahnnummer = '".$bahn."'"); 
+$database = "true";
+if ($database == "true")
+{
+mysqli_query($conn ,"INSERT INTO ".$save_name." SET name = '".$bild_name."',type = 'image/png' ,bildlink = '".$file."',bahnnummer = '".$bahn."'"); 
+}
 print $success ? $file : 'Unable to save the file.';
 ?>
