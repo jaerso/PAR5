2125632ef790f3d5eb2a5151d11d6ee07fea49b5
DominikSommer <29626701+DominikSommer@users.noreply.github.com>
2017-09-29 15:01:29 +0200

    Kommentar fehler behoben

--- includes/bilderoutput.inc.php
+++ includes/bilderoutput.inc.php
@@ -6,40 +6,44 @@ if(isset($_GET['bahn'])){
     $bahnnummer = $_GET['bahn'];
 
     $data_SQL="SELECT * FROM images WHERE bahnnummer=$bahnnummer"; //Übergabe der ID: auch das klappt, da die Textfelder korrekt angezeigt werden
-    //$checkID='';
+   // $checkID=NULL;
     $result = mysqli_query($conn, $data_SQL);
     while($data=mysqli_fetch_assoc($result))
     {
     echo "<img src='".$data['bildlink']."' alt='Bild'>";
     //Comment
-    //$sqlComment= "SELECT * FROM comments WHERE imgid='".$data['bahnnummer']."'";
-  //  $resultComment=mysqli_query($conn,$sqlComment);
-   // while($row=mysqli_fetch_assoc($resultComment)){
       $bildid= $data['id'];
         getComments($conn,$bildid);
         echo "<br>";
         //print_r('Ausgabe');
    // }
    
-    
+    //comments($conn,$bildid,$data);
     if(isset($_SESSION['u_id'])){
-        echo "<form action='".setComments($conn)."' method='POST'>
-        <input type='hidden' name='u_id' value='".$_SESSION['u_id']."'>
-        <input type='hidden' name='u_uid' value='".$_SESSION['u_uid']."'>
-        <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
-        <input type='hidden' name='imgid' value='".$data['id']."'>";
-        //<input type='hidden' name='checkID' value='". $checkID. "'>
-       echo" <textarea name='message'></textarea><br>
-        <button type='submit' name='commentSubmit'>Kommentieren</button>
-        </form>
-        <br>";
-       
-    } else{
-    echo "Du musst eingeloggt sein, um zu kommentieren
-    <br><br>";
-    }
+      echo "<form action='".setComments($conn,$bildid)."' method='POST'>
+      <input type='hidden' name='u_id' value='".$_SESSION['u_id']."'>
+      <input type='hidden' name='u_uid' value='".$_SESSION['u_uid']."'>
+      <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
+      <input type='hidden' name='imgid' value='".$data['id']."'>";
+     // <input type='hidden' name='checkID' value='". $checkID. "'>
+      echo "<textarea name='message'></textarea><br>
+      <button type='submit' name='commentSubmit' value='".$data['id']."'>Kommentieren</button>
+      </form>
+      <br>";
+     
+  } else{
+  echo "Du musst eingeloggt sein, um zu kommentieren
+  <br><br>";
+  }
+  
   //  header("Location: ../index.php?page=gallery&bahn=$bahnnummer");
   //unset($_POST);
     }  
 }
+
+
+
+/*function comments($conn,$bildid,$data){
+  
+}*/
 ?>
