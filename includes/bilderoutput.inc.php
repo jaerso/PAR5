d8583888b6a1c884e31db4baae2bd45b3a79daf3
DominikSommer <29626701+DominikSommer@users.noreply.github.com>
2017-10-02 13:12:14 +0200

    Kommentare weiterleiten/aktualisieren voll funktionsfähig

--- includes/bilderoutput.inc.php
+++ includes/bilderoutput.inc.php
@@ -46,4 +46,3 @@ if(isset($_GET['bahn'])){
 }
 
 ?>
-
--- includes/comments.inc.php
+++ includes/comments.inc.php
@@ -31,6 +31,10 @@ if(empty($checkID)){
 
                 $sql = "INSERT INTO comments (u_id, u_uid, date, message, imgid) VALUES('$u_id','$u_uid', '$date', '$message', '$imgid')";
                 $result= mysqli_query($conn,$sql);
+               echo" <script language='javascript'
+                type='text/javascript'>
+                document.location='index.php?page=gallery&bahn=$bahn';
+                </script>";
                 /*print_r($_POST['commentSubmit']);
             unset($_POST['commentSubmit']);
             print_r($_POST['commentSubmit']);*/
@@ -76,10 +80,10 @@ function getComments($conn,$bildid){
             echo "</p>";
             if(isset($_SESSION['u_id'])){
                 if($_SESSION['u_id']== $row2['user_id']){
-                    $_SESSION['bahn']= $_GET['bahn'];
+                    $bahn= $_GET['bahn'];
                     
                     //echo "$bahn";
-                        echo "<form class='delete-form' method='POST' action='".deleteComments($conn,$bildid)."'>
+                        echo "<form class='delete-form' method='POST' action='".deleteComments($conn,$bahn)."'>
                         <input type='hidden' name='cid' value='".$row['cid']."'>
                         <button type='submit' name='commentDelete'>Löschen</button>
                         </form>
@@ -88,6 +92,7 @@ function getComments($conn,$bildid){
                         <input type='hidden' name='u_id' value='".$row['u_id']."'>
                         <input type='hidden' name='date' value='".$row['date']."'>
                         <input type='hidden' name='message' value='".$row['message']."'>
+                        <input type='hidden' name='bahn' value='".$bahn."'>
                         <button>Bearbeiten</button>
                     
                         </form>";
@@ -111,17 +116,20 @@ function getComments($conn,$bildid){
 }
 
 
-function editComments($conn){
+function editComments($conn,$bahn){
     if(isset($_POST['commentSubmit'])){
     $cid=$_POST['cid'];
-    $u_uid=$_POST['u_uid'];
+    $u_id=$_POST['u_id'];
     $date=$_POST['date'];
     $message=$_POST['message'];
-    $bahn=$_SESSION['bahn'];
+    $bahn=$_POST['bahn'];
     
-    $sql = "UPDATE comments SET message='$bahn' WHERE cid='$cid'";
+    $sql = "UPDATE comments SET message='$message' WHERE cid='$cid'";
     $result = mysqli_query($conn,$sql);
-    header("Location: index.php?page=gallery&bahn=$bahn");
+    echo" <script language='javascript'
+    type='text/javascript'>
+    document.location='../index.php?page=gallery&bahn=$bahn';
+    </script>";
     
     }
     }
@@ -140,13 +148,16 @@ function editComments($conn){
         }
         }*/
 
-    function deleteComments($conn,$bildid){
+    function deleteComments($conn,$bahn){
         if(isset($_POST['commentDelete'])){
             $cid=$_POST['cid'];
             
             $sql = "DELETE FROM comments WHERE cid='$cid'";
             $result = mysqli_query($conn,$sql);
-            header("Location: index.php?page=gallery&bahn=$bildid");
+            echo" <script language='javascript'
+            type='text/javascript'>
+            document.location='index.php?page=gallery&bahn=$bahn';
+            </script>";
             }
     }
 
